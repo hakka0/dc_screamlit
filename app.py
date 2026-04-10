d4227ee8156643b5dbf3bf23ae87f50f335e5727
hakka0 <215169176+hakka0@users.noreply.github.com>
2026-04-11 03:53:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -100,6 +100,7 @@ def load_data_from_oracle():
         """
         
         with connection.cursor() as cursor:
+            cursor.arraysize = 10000
             
             cursor.execute(query, [cutoff_str])
             columns = [col[0] for col in cursor.description]
